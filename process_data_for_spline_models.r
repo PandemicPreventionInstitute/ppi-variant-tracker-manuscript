30be74df7b98dba58f7a2f260a6c11469dad82e6
zsusswein@rockfound.org <zsusswein@rockfound.org>
2023-02-01 13:57:41 +0000

    Clean up fits and order
    Kaitlyn, can you pay particular attention to whether the brier score calc is right?

--- process_data_for_spline_models.r
+++ process_data_for_spline_models.r
@@ -14,10 +14,6 @@ install.packages("arrow", repos = "https://packagemanager.rstudio.com/all/__linu
 
 # COMMAND ----------
 
-reference_dates <- c('2022-04-30', '2022-05-16', '2022-05-27', '2022-06-04', '2022-06-27', '2022-07-01')
-
-# COMMAND ----------
-
 # MAGIC %py
 # MAGIC 
 # MAGIC from  pyspark.sql.functions import input_file_name
@@ -28,7 +24,7 @@ reference_dates <- c('2022-04-30', '2022-05-16', '2022-05-27', '2022-06-04', '20
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC 
+# MAGIC -- Pull in all the all the distinct region-country combinations from the metadata
 # MAGIC CREATE OR REPLACE TEMP VIEW region_map AS SELECT DISTINCT TRIM(UPPER(regexp_extract(location, '([A-Za-z]* ([A-Za-z]*( )?)?)', 0))) AS region, country FROM raw_metadata
 
 # COMMAND ----------
@@ -46,19 +42,14 @@ library(splines)
 
 # COMMAND ----------
 
+# Pull the region-country combos into memory and map to iso3 code, dropping NAs
 region_map <- as_tibble(SparkR::as.data.frame(SparkR::sql('SELECT * FROM region_map;')))
-
-# COMMAND ----------
-
 region_map$code <- countrycode(region_map$country, origin = 'country.name', destination = 'iso3c')
-
-# COMMAND ----------
-
 region_map <- as_tibble(region_map) %>% filter(!is.na(code)) %>% select(-country)
 
 # COMMAND ----------
 
-region_map
+reference_dates <- c('2022-04-30', '2022-05-16', '2022-05-27', '2022-06-04', '2022-06-27', '2022-07-01')
 
 # COMMAND ----------
 
@@ -66,6 +57,9 @@ for (reference_date in reference_dates){
   
   print(paste('Fitting model for:', reference_date))
   
+  # If rerunning for some reason, skip already computed results
+  if (!file.exists(paste0("/dbfs/mnt/ppi-test/validation/spline_fits/fitted_model/", reference_date, ".rds"))){
+    
   # Read in lineage file
   df_full <- read_csv(paste0('/dbfs/mnt/ppi-test/validation/validation_data/lineage_t_', reference_date, '.csv')) %>%
       left_join(region_map, by = 'code') %>%
@@ -89,35 +83,43 @@ for (reference_date in reference_dates){
   
   # Save model
   saveRDS(fit, file=paste0("/dbfs/mnt/ppi-test/validation/spline_fits/fitted_model/", reference_date, ".rds"))
-  
-  # pass into emmeans
-  rg_nnet <- ref_grid(fit, at = list(t = max_t_calibration))
+    
+  # pass into emmeans to get marginal slopes at t = t_max
+  # This is directly the logic on line 497 of https://github.com/tomwenseleers/LineageExplorer/blob/main/global%20analysis.R
+  rg_nnet <- ref_grid(fit, 
+                      at = list(t = max_t_calibration, 
+                                # We only want to calculate main lineages of interest here
+                                lineage = c('BA.2', 'BA.1', 'BA.2.12.1', 'BA.4', 'BA.5')))
   em_nnet <- emmeans(rg_nnet,
-                   specs = pairwise ~ t|lineage)
-  
-  # regrid to get coefficients as logit
-  em_nnet_logit <- regrid(em_nnet[[1]],
-                        transform = "logit")
-  
+                   specs = ~ t|lineage,
+                    mode = 'latent')
+    
   # Get contrasts
-  contrasts <- as_tibble(pairs(em_nnet_logit, simple = 'each', combine = TRUE)) %>% 
-                filter(grepl("BA.2 - ", contrast, fixed = TRUE))
-  
-  write_csv(contrasts, paste0('/dbfs/mnt/ppi-test/validation/spline_fits/contrast/', reference_date, '.csv'))
-  
+  contrasts <- as_tibble(pairs(em_nnet, simple = 'each', combine = TRUE)) %>% 
+                filter(grepl("BA.2 - ", contrast, fixed = TRUE)) %>%
+                select(-lineage)
+
+  arrow::write_parquet(contrasts, paste0('/dbfs/mnt/ppi-test/validation/spline_fits/contrast/', reference_date, '.parquet'))
+    
   # Use full to include fitting period + forecast period
   newd <- df_full %>% 
     select(t, country, region) %>% 
     distinct()
   
-  # Get brier score
+  # Get brier score and predicted values
   brier_score_df <- bind_cols(predict(fit, newd, type = 'p'), newd) %>% 
     pivot_longer(cols = !c(region, country, t), names_to = 'lineage', values_to = 'p') %>% 
     left_join(df_full) %>%
     mutate(fitted_count = tot_seq * p,
          brier_score = (fitted_count - n_seq)**2 / tot_seq) %>%
-    select(region, country, collection_date, t, p, fitted_count, brier_score, tot_seq, n_seq, p_lineage_week)
+    select(region, country, collection_date, t, period, p, fitted_count, brier_score, tot_seq, n_seq, p_lineage_week)
   
-  write_csv(brier_score_df, paste0('/dbfs/mnt/ppi-test/validation/spline_fits/brier_score/', reference_date, '.csv'))
+  arrow::write_parquet(brier_score_df, paste0('/dbfs/mnt/ppi-test/validation/spline_fits/brier_score/', reference_date, '.parquet'))
   
+
+  }
 }
+
+# COMMAND ----------
+
+
